688d34517c9910f807de980cb65891ad5770efef
Raphael Khoury <raphael.khoury@uqo.ca>
2023-04-24 16:40:21 -0400

    Update 13-AESkey_3people.cpp

--- A-Initial Programs/13-AESkey_3people.cpp
+++ A-Initial Programs/13-AESkey_3people.cpp
@@ -1,52 +1,58 @@
 #include <openssl/aes.h>
+#include <openssl/rand.h>
 #include <iostream>
-#include <cstring>
+#include <string>
+#include <vector>
 
-// Function to generate a random key
-void generateRandomKey(unsigned char *key, int keyLength) {
-    FILE *urandom = fopen("/dev/urandom", "r");
-    fread(key, 1, keyLength, urandom);
-    fclose(urandom);
+// The key used for AES encryption. This should be kept secret.
+std::vector<unsigned char> key = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+
+// The initialization vector used for AES encryption.
+std::vector<unsigned char> iv = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+
+// Function to encrypt a given string using AES and return the ciphertext.
+std::vector<unsigned char> aes_encrypt(const std::string& plaintext)
+{
+    // Create a buffer to hold the ciphertext.
+    std::vector<unsigned char> ciphertext(plaintext.size() + AES_BLOCK_SIZE);
+
+    // Create an AES encryption context.
+    AES_KEY aes_key;
+    AES_set_encrypt_key(key.data(), 128, &aes_key);
+
+    // Encrypt the plaintext using AES in CBC mode.
+    AES_cbc_encrypt((const unsigned char*) plaintext.data(), ciphertext.data(), plaintext.size(), &aes_key, iv.data(), AES_ENCRYPT);
+
+    return ciphertext;
 }
 
-int main() {
-    const unsigned char plaintext[] = "Hello World";
-    const unsigned char iv[] = "initialvector123";
+int main()
+{
+    // Get the plaintext to encrypt.
+    std::string plaintext = "Hello, world!";
 
-    // Generate a random 128-bit AES key
-    const int keyLength = 16; // 128-bit key
-    unsigned char key[keyLength];
-    generateRandomKey(key, keyLength);
+    // Encrypt the plaintext using AES.
+    std::vector<unsigned char> ciphertext = aes_encrypt(plaintext);
 
-    // Encrypt the plaintext message using AES-CBC with the generated key
-    unsigned char ciphertext[AES_BLOCK_SIZE];
-    memset(ciphertext, 0, AES_BLOCK_SIZE);
+    // Send the ciphertext to three different persons.
+    // Here, we are just printing the ciphertext to the console.
+    std::cout << "Ciphertext 1: " << std::hex;
+    for (auto c : ciphertext) {
+        std::cout << (int) c << " ";
+    }
+    std::cout << std::dec << std::endl;
 
-    AES_KEY aesKey;
-    AES_set_encrypt_key(key, keyLength*8, &aesKey);
-    AES_cbc_encrypt(plaintext, ciphertext, sizeof(plaintext), &aesKey, iv, AES_ENCRYPT);
+    std::cout << "Ciphertext 2: " << std::hex;
+    for (auto c : ciphertext) {
+        std::cout << (int) c << " ";
+    }
+    std::cout << std::dec << std::endl;
 
-    // Store the encrypted message in three different variables
-    unsigned char johnCiphertext[AES_BLOCK_SIZE];
-    unsigned char mikeCiphertext[AES_BLOCK_SIZE];
-    unsigned char timCiphertext[AES_BLOCK_SIZE];
-
-    AES_KEY johnKey, mikeKey, timKey;
-
-    // Generate a unique key for John
-    generateRandomKey(johnKey.rd_key, keyLength);
-    AES_set_encrypt_key(johnKey.rd_key, keyLength*8, &johnKey);
-    AES_cbc_encrypt(ciphertext, johnCiphertext, sizeof(ciphertext), &johnKey, iv, AES_ENCRYPT);
-
-    // Generate a unique key for Mike
-    generateRandomKey(mikeKey.rd_key, keyLength);
-    AES_set_encrypt_key(mikeKey.rd_key, keyLength*8, &mikeKey);
-    AES_cbc_encrypt(ciphertext, mikeCiphertext, sizeof(ciphertext), &mikeKey, iv, AES_ENCRYPT);
-
-    // Generate a unique key for Tim
-    generateRandomKey(timKey.rd_key, keyLength);
-    AES_set_encrypt_key(timKey.rd_key, keyLength*8, &timKey);
-    AES_cbc_encrypt(ciphertext, timCiphertext, sizeof(ciphertext), &timKey, iv, AES_ENCRYPT);
+    std::cout << "Ciphertext 3: " << std::hex;
+    for (auto c : ciphertext) {
+        std::cout << (int) c << " ";
+    }
+    std::cout << std::dec << std::endl;
 
     return 0;
 }
